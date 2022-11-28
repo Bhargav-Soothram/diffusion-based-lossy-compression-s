d60f4c60199559748bb4abc03b863117001d95e4
Bhargav-Soothram <bhargav.soothram@gmail.com>
2022-11-28 00:19:55 -0500

    completed pipeline

--- models/diffusion.py
+++ models/diffusion.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn as nn
+from utils import HyperAnalysisTransform, HyperSynthesisTransform
 
 
 def get_timestep_embedding(timesteps, embedding_dim):
@@ -190,156 +191,156 @@ class AttnBlock(nn.Module):
         return x+h_
 
 
-class Model(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
-        ch, out_ch, ch_mult = config.model.ch, config.model.out_ch, tuple(config.model.ch_mult)
-        num_res_blocks = config.model.num_res_blocks
-        attn_resolutions = config.model.attn_resolutions
-        dropout = config.model.dropout
-        in_channels = config.model.in_channels
-        resolution = config.data.image_size
-        resamp_with_conv = config.model.resamp_with_conv
-        num_timesteps = config.diffusion.num_diffusion_timesteps
+# class Model(nn.Module):
+#     def __init__(self, config):
+#         super().__init__()
+#         self.config = config
+#         ch, out_ch, ch_mult = config.model.ch, config.model.out_ch, tuple(config.model.ch_mult)
+#         num_res_blocks = config.model.num_res_blocks
+#         attn_resolutions = config.model.attn_resolutions
+#         dropout = config.model.dropout
+#         in_channels = config.model.in_channels
+#         resolution = config.data.image_size
+#         resamp_with_conv = config.model.resamp_with_conv
+#         num_timesteps = config.diffusion.num_diffusion_timesteps
         
-        if config.model.type == 'bayesian':
-            self.logvar = nn.Parameter(torch.zeros(num_timesteps))
+#         if config.model.type == 'bayesian':
+#             self.logvar = nn.Parameter(torch.zeros(num_timesteps))
         
-        self.ch = ch
-        self.temb_ch = self.ch*4
-        self.num_resolutions = len(ch_mult)
-        self.num_res_blocks = num_res_blocks
-        self.resolution = resolution
-        self.in_channels = in_channels
+#         self.ch = ch
+#         self.temb_ch = self.ch*4
+#         self.num_resolutions = len(ch_mult)
+#         self.num_res_blocks = num_res_blocks
+#         self.resolution = resolution
+#         self.in_channels = in_channels
 
-        # timestep embedding
-        self.temb = nn.Module()
-        self.temb.dense = nn.ModuleList([
-            torch.nn.Linear(self.ch,
-                            self.temb_ch),
-            torch.nn.Linear(self.temb_ch,
-                            self.temb_ch),
-        ])
+#         # timestep embedding
+#         self.temb = nn.Module()
+#         self.temb.dense = nn.ModuleList([
+#             torch.nn.Linear(self.ch,
+#                             self.temb_ch),
+#             torch.nn.Linear(self.temb_ch,
+#                             self.temb_ch),
+#         ])
 
-        # downsampling
-        self.conv_in = torch.nn.Conv2d(in_channels,
-                                       self.ch,
-                                       kernel_size=3,
-                                       stride=1,
-                                       padding=1)
+#         # downsampling
+#         self.conv_in = torch.nn.Conv2d(in_channels,
+#                                        self.ch,
+#                                        kernel_size=3,
+#                                        stride=1,
+#                                        padding=1)
 
-        curr_res = resolution
-        in_ch_mult = (1,)+ch_mult
-        self.down = nn.ModuleList()
-        block_in = None
-        for i_level in range(self.num_resolutions):
-            block = nn.ModuleList()
-            attn = nn.ModuleList()
-            block_in = ch*in_ch_mult[i_level]
-            block_out = ch*ch_mult[i_level]
-            for i_block in range(self.num_res_blocks):
-                block.append(ResnetBlock(in_channels=block_in,
-                                         out_channels=block_out,
-                                         temb_channels=self.temb_ch,
-                                         dropout=dropout))
-                block_in = block_out
-                if curr_res in attn_resolutions:
-                    attn.append(AttnBlock(block_in))
-            down = nn.Module()
-            down.block = block
-            down.attn = attn
-            if i_level != self.num_resolutions-1:
-                down.downsample = Downsample(block_in, resamp_with_conv)
-                curr_res = curr_res // 2
-            self.down.append(down)
+#         curr_res = resolution
+#         in_ch_mult = (1,)+ch_mult
+#         self.down = nn.ModuleList()
+#         block_in = None
+#         for i_level in range(self.num_resolutions):
+#             block = nn.ModuleList()
+#             attn = nn.ModuleList()
+#             block_in = ch*in_ch_mult[i_level]
+#             block_out = ch*ch_mult[i_level]
+#             for i_block in range(self.num_res_blocks):
+#                 block.append(ResnetBlock(in_channels=block_in,
+#                                          out_channels=block_out,
+#                                          temb_channels=self.temb_ch,
+#                                          dropout=dropout))
+#                 block_in = block_out
+#                 if curr_res in attn_resolutions:
+#                     attn.append(AttnBlock(block_in))
+#             down = nn.Module()
+#             down.block = block
+#             down.attn = attn
+#             if i_level != self.num_resolutions-1:
+#                 down.downsample = Downsample(block_in, resamp_with_conv)
+#                 curr_res = curr_res // 2
+#             self.down.append(down)
 
-        # middle
-        self.mid = nn.Module()
-        self.mid.block_1 = ResnetBlock(in_channels=block_in,
-                                       out_channels=block_in,
-                                       temb_channels=self.temb_ch,
-                                       dropout=dropout)
-        self.mid.attn_1 = AttnBlock(block_in)
-        self.mid.block_2 = ResnetBlock(in_channels=block_in,
-                                       out_channels=block_in,
-                                       temb_channels=self.temb_ch,
-                                       dropout=dropout)
+#         # middle
+#         self.mid = nn.Module()
+#         self.mid.block_1 = ResnetBlock(in_channels=block_in,
+#                                        out_channels=block_in,
+#                                        temb_channels=self.temb_ch,
+#                                        dropout=dropout)
+#         self.mid.attn_1 = AttnBlock(block_in)
+#         self.mid.block_2 = ResnetBlock(in_channels=block_in,
+#                                        out_channels=block_in,
+#                                        temb_channels=self.temb_ch,
+#                                        dropout=dropout)
 
-        # upsampling
-        self.up = nn.ModuleList()
-        for i_level in reversed(range(self.num_resolutions)):
-            block = nn.ModuleList()
-            attn = nn.ModuleList()
-            block_out = ch*ch_mult[i_level]
-            skip_in = ch*ch_mult[i_level]
-            for i_block in range(self.num_res_blocks+1):
-                if i_block == self.num_res_blocks:
-                    skip_in = ch*in_ch_mult[i_level]
-                block.append(ResnetBlock(in_channels=block_in+skip_in,
-                                         out_channels=block_out,
-                                         temb_channels=self.temb_ch,
-                                         dropout=dropout))
-                block_in = block_out
-                if curr_res in attn_resolutions:
-                    attn.append(AttnBlock(block_in))
-            up = nn.Module()
-            up.block = block
-            up.attn = attn
-            if i_level != 0:
-                up.upsample = Upsample(block_in, resamp_with_conv)
-                curr_res = curr_res * 2
-            self.up.insert(0, up)  # prepend to get consistent order
+#         # upsampling
+#         self.up = nn.ModuleList()
+#         for i_level in reversed(range(self.num_resolutions)):
+#             block = nn.ModuleList()
+#             attn = nn.ModuleList()
+#             block_out = ch*ch_mult[i_level]
+#             skip_in = ch*ch_mult[i_level]
+#             for i_block in range(self.num_res_blocks+1):
+#                 if i_block == self.num_res_blocks:
+#                     skip_in = ch*in_ch_mult[i_level]
+#                 block.append(ResnetBlock(in_channels=block_in+skip_in,
+#                                          out_channels=block_out,
+#                                          temb_channels=self.temb_ch,
+#                                          dropout=dropout))
+#                 block_in = block_out
+#                 if curr_res in attn_resolutions:
+#                     attn.append(AttnBlock(block_in))
+#             up = nn.Module()
+#             up.block = block
+#             up.attn = attn
+#             if i_level != 0:
+#                 up.upsample = Upsample(block_in, resamp_with_conv)
+#                 curr_res = curr_res * 2
+#             self.up.insert(0, up)  # prepend to get consistent order
 
-        # end
-        self.norm_out = Normalize(block_in)
-        self.conv_out = torch.nn.Conv2d(block_in,
-                                        out_ch,
-                                        kernel_size=3,
-                                        stride=1,
-                                        padding=1)
+#         # end
+#         self.norm_out = Normalize(block_in)
+#         self.conv_out = torch.nn.Conv2d(block_in,
+#                                         out_ch,
+#                                         kernel_size=3,
+#                                         stride=1,
+#                                         padding=1)
 
-    def forward(self, x, t):
-        assert x.shape[2] == x.shape[3] == self.resolution
+#     def forward(self, x, t):
+#         assert x.shape[2] == x.shape[3] == self.resolution
 
-        # timestep embedding
-        temb = get_timestep_embedding(t, self.ch)
-        temb = self.temb.dense[0](temb)
-        temb = nonlinearity(temb)
-        temb = self.temb.dense[1](temb)
+#         # timestep embedding
+#         temb = get_timestep_embedding(t, self.ch)
+#         temb = self.temb.dense[0](temb)
+#         temb = nonlinearity(temb)
+#         temb = self.temb.dense[1](temb)
 
-        # downsampling
-        hs = [self.conv_in(x)]
-        for i_level in range(self.num_resolutions):
-            for i_block in range(self.num_res_blocks):
-                h = self.down[i_level].block[i_block](hs[-1], temb)
-                if len(self.down[i_level].attn) > 0:
-                    h = self.down[i_level].attn[i_block](h)
-                hs.append(h)
-            if i_level != self.num_resolutions-1:
-                hs.append(self.down[i_level].downsample(hs[-1]))
+#         # downsampling
+#         hs = [self.conv_in(x)]
+#         for i_level in range(self.num_resolutions):
+#             for i_block in range(self.num_res_blocks):
+#                 h = self.down[i_level].block[i_block](hs[-1], temb)
+#                 if len(self.down[i_level].attn) > 0:
+#                     h = self.down[i_level].attn[i_block](h)
+#                 hs.append(h)
+#             if i_level != self.num_resolutions-1:
+#                 hs.append(self.down[i_level].downsample(hs[-1]))
 
-        # middle
-        h = hs[-1]
-        h = self.mid.block_1(h, temb)
-        h = self.mid.attn_1(h)
-        h = self.mid.block_2(h, temb)
+#         # middle
+#         h = hs[-1]
+#         h = self.mid.block_1(h, temb)
+#         h = self.mid.attn_1(h)
+#         h = self.mid.block_2(h, temb)
 
-        # upsampling
-        for i_level in reversed(range(self.num_resolutions)):
-            for i_block in range(self.num_res_blocks+1):
-                h = self.up[i_level].block[i_block](
-                    torch.cat([h, hs.pop()], dim=1), temb)
-                if len(self.up[i_level].attn) > 0:
-                    h = self.up[i_level].attn[i_block](h)
-            if i_level != 0:
-                h = self.up[i_level].upsample(h)
+#         # upsampling
+#         for i_level in reversed(range(self.num_resolutions)):
+#             for i_block in range(self.num_res_blocks+1):
+#                 h = self.up[i_level].block[i_block](
+#                     torch.cat([h, hs.pop()], dim=1), temb)
+#                 if len(self.up[i_level].attn) > 0:
+#                     h = self.up[i_level].attn[i_block](h)
+#             if i_level != 0:
+#                 h = self.up[i_level].upsample(h)
 
-        # end
-        h = self.norm_out(h)
-        h = nonlinearity(h)
-        h = self.conv_out(h)
-        return h
+#         # end
+#         h = self.norm_out(h)
+#         h = nonlinearity(h)
+#         h = self.conv_out(h)
+#         return h
 
 
 # New model class that includes semantic embeddings
@@ -356,7 +357,8 @@ class Model(nn.Module):
         resamp_with_conv = config.model.resamp_with_conv
         num_timesteps = config.diffusion.num_diffusion_timesteps
         
-        embed_ch, self.is_embed = config.embed.ch, ch_mult, config.embed.is_embed
+        self.encoder_ch = tuple(config.model.encoder_ch)
+
         if config.model.type == 'bayesian':
             self.logvar = nn.Parameter(torch.zeros(num_timesteps))
         
@@ -376,18 +378,38 @@ class Model(nn.Module):
                             self.temb_ch),
         ])
 
-        # semantic embedding
-        in_ch_mult = (1,)+ch_mult
-        self.sembed = nn.ModuleList()
-        for i_level in range(self.num_resolutions):
-            block_in = ch*reversed(ch_mult)[i_level]
-            block_out = ch*reversed(in_ch_mult)[i_level]
-            if self.is_embed[i_level] == 1:
-                self.sembed.append(ResnetBlock(in_channels=block_in,
-                                         out_channels=block_in,
+        # encoder block
+        self.encoder = nn.ModuleList()
+        in_encoder_ch = (1,)+self.encoder_ch
+        for i_level in range(len(self.encoder_ch)):
+            enc_block_in = ch*in_encoder_ch[i_level]
+            enc_block_out = ch*self.encoder_ch[i_level]
+            enc_block = nn.Module()
+            enc_block.block = ResnetBlock(in_channels=enc_block_in,
+                                         out_channels=enc_block_out,
                                          temb_channels=self.temb_ch, 
-                                         dropout=dropout))
+                                         dropout=dropout)
+            enc_block_in = enc_block_out
+            if i_level != len(self.encoder_ch)-1:
+                enc_block.downsample = Downsample(enc_block_in, resamp_with_conv)
+            self.encoder.append(enc_block)
                 
+        # embedder block
+        in_ch_mult = (1,)+ch_mult
+        self.embedder = nn.ModuleList()
+        for i_level in reversed(range(len(self.encoder_ch))):
+            emb_block_in = ch*in_encoder_ch[i_level]
+            emb_block_out = ch*self.encoder_ch[i_level]
+            emb_block = nn.Module()
+            emb_block.block = ResnetBlock(in_channels=emb_block_in,
+                                         out_channels=emb_block_out,
+                                         temb_channels=self.temb_ch, 
+                                         dropout=dropout)
+            emb_block_in = emb_block_out
+            if i_level != 0:
+                emb_block.upsample = Upsample(enc_block_in, resamp_with_conv)
+            self.embedder.append(emb_block)
+
         # downsampling
         self.conv_in = torch.nn.Conv2d(in_channels,
                                        self.ch,
@@ -465,7 +487,7 @@ class Model(nn.Module):
                                         stride=1,
                                         padding=1)
 
-    def forward(self, x, t):
+    def forward(self, x, x_original, t):
         assert x.shape[2] == x.shape[3] == self.resolution
 
         # timestep embedding
@@ -474,21 +496,35 @@ class Model(nn.Module):
         temb = nonlinearity(temb)
         temb = self.temb.dense[1](temb)
 
+        # encoding the actual image for conditioning
+        z = None
+        for i_level in range(len(self.encoder)):
+            z = self.encoder[i_level].block(x_original, temb=None, semb=None)
+            z = self.encoder[i_level].downsample(z)
+
+        hyper_analysis_transform = HyperAnalysisTransform()
+        y = hyper_analysis_transform(z) # hyper-latents
+
+        compressed_y = torch.round(y) # quantized hyper-latents
+        
+        hyper_synthesis_transform = HyperSynthesisTransform()
+        z_hat = hyper_synthesis_transform(compressed_y) # reconstructed latents
+
         # semantic embedding
-        zs = [0] # from the encoder, to be coded!
-        for i_level in range(self.num_resolutions):
-            if self.is_embed[i_level] == 1:
-                z = self.sembed[i_level](zs[-1], temb=None, semb=None) # change the ResnetBlock function to not do time embedding everytime!
-                z = torch.nn.functional.interpolate(z, scale_factor=2.0, mode="nearest")
-                zs.append(z)
-            else:
-                zs.append(None)
+        z_recon = [z_hat] # from the encoder, to be coded!
+        for i_level in range(len(self.encoder)):
+            z_h = self.embedder[i_level].block(z_recon[-1], temb=None, semb=None)
+            z_h = self.embedder[i_level].upsample(z_h)
+            z_recon.append(z_h)
 
         # downsampling
         hs = [self.conv_in(x)]
         for i_level in range(self.num_resolutions):
             for i_block in range(self.num_res_blocks):
-                h = self.down[i_level].block[i_block](hs[-1], temb, zs[i_level])
+                if i_level < len(z_recon):
+                    h = self.down[i_level].block[i_block](hs[-1], temb, z_recon[i_level])
+                else:
+                    h = self.down[i_level].block[i_block](hs[-1], temb, None)
                 if len(self.down[i_level].attn) > 0:
                     h = self.down[i_level].attn[i_block](h)
                 hs.append(h)
@@ -516,4 +552,3 @@ class Model(nn.Module):
         h = nonlinearity(h)
         h = self.conv_out(h)
         return h
-
